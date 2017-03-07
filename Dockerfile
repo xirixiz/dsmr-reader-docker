19882fb0bf00561b2153af798187a78f5316ea8d
Bram <spam@rootrulez.com>
2017-03-07 11:40:14 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM ubuntu:16.04
+MAINTAINER Bram van Dartel <root@rootrulez.com>
+
+ENV DEBIAN_FRONTEND="noninteractive"
+SHELL ["/bin/bash", "-c"]
+
+RUN echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/02apt-speedup && \
+    echo "Acquire::http {No-Cache=True;};" > /etc/apt/apt.conf.d/no-cache && \
+    apt-get -q update && \
+    apt-get install -qy \
+      python3 \
+      python3-dev \
+      python3-pip \
+      python3-virtualenv \
+      postgresql \
+      virtualenvwrapper \
+      supervisor \
+      cu \
+      git \
+      jq \
+      nginx \
+      curl \
+      sudo \
+      wget
+
+ADD scripts/startup.sh /
+RUN chmod 755 /startup.sh
+
+COPY scripts/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
+RUN ln -sf /dev/stdout /var/log/nginx/access.log && \
+    ln -sf /dev/stderr /var/log/nginx/error.log && \
+    rm /etc/nginx/sites-enabled/default
+
+EXPOSE 80 443
+
+ENTRYPOINT ["/startup.sh"]
---
+++ scripts/startup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# UPDATE PIP
+pip3 install --upgrade pip
+
+# ADD ROOT TO DIALOUT GROUP
+usermod -a -G dialout root 
+
+# GIT CLONE
+git clone https://github.com/dennissiemensma/dsmr-reader.git /root/dsmr-reader
+
+# VIRTUALENV
+mkdir /root/.virtualenvs
+virtualenv /root/.virtualenvs/dsmrreader --no-site-packages --python python3
+source /root/.virtualenvs/dsmrreader/bin/activate
+
+# DJANGO REQUIREMENTS
+pip3 install -r /root/dsmr-reader/dsmrreader/provisioning/requirements/base.txt
+pip3 install psycopg2
+
+# NGINX REQUIREMENTS
+mkdir -p /var/www/dsmrreader/static
+
+# RUN django tasks
+cp /root/dsmr-reader/dsmrreader/provisioning/django/postgresql.py /root/dsmr-reader/dsmrreader/settings.py
+sed -i 's/localhost/dsmrdb/g' /root/dsmr-reader/dsmrreader/settings.py
+/root/dsmr-reader/manage.py migrate
+/root/dsmr-reader/manage.py collectstatic --noinput
+
+# NGINX Config
+cp /root/dsmr-reader/dsmrreader/provisioning/nginx/dsmr-webinterface /etc/nginx/sites-enabled/
+  
+# START SERVICES
+/usr/sbin/nginx
+/usr/bin/supervisord -n
