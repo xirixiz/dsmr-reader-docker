0e73b9e3748ea0356aaeebc9bfbf41e1a538f9a7
Bram van Dartel <spam@rootrulez.com>
2018-07-06 13:03:08 +0200

    Update entrypoint.sh
    Fixed indents

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -17,14 +17,13 @@ cmd="$cmd -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t 1"
 
 timeout=60
 while ! $cmd >/dev/null 2>&1; do
-    timeout=$(expr $timeout - 1)
-    if [[ $timeout -eq 0 ]]; then
-        echo
-        echo "Could not connect to database server. Aborting..."
-        return 1
-    fi
-    echo -n "."
-    sleep 1
+  timeout=$(expr $timeout - 1)
+  if [[ $timeout -eq 0 ]]; then
+    echo "Could not connect to database server. Aborting..."
+    return 1
+  fi
+  echo -n "."
+  sleep 1
 done
 echo
 
@@ -36,26 +35,23 @@ su dsmr -c "python3 manage.py collectstatic --noinput"
 # python3 manage.py for example. Keep in mind that the
 # WORKDIR is set to /home/dsmr/app.
 if [ -n "$COMMAND" ]; then
-	echo "ENTRYPOINT: Executing override command"
-	exec $COMMAND
+  echo "ENTRYPOINT: Executing override command"
+  exec $COMMAND
 fi
 
-if [ -z "${DSMR_USER}" ] || [ -z "$DSMR_EMAIL" ] || \
-   [ -z "${DSMR_PASSWORD}" ]; then
-	echo "DSMR web credentials not set. Exiting."
-	exit 1
+if [ -z "${DSMR_USER}" ] || [ -z "$DSMR_EMAIL" ] || [ -z "${DSMR_PASSWORD}" ]; then
+  echo "DSMR web credentials not set. Exiting."
+  exit 1
 fi
 
 # Create an admin user
 su dsmr -c "python3 manage.py shell -i python << PYTHON
 from django.contrib.auth.models import User
 if not User.objects.filter(username='${DSMR_USER}'):
-	User.objects.create_superuser(
-    '${DSMR_USER}', '${DSMR_EMAIL}', '${DSMR_PASSWORD}'
-  )
-	print('${DSMR_USER} created')
+  User.objects.create_superuser('${DSMR_USER}', '${DSMR_EMAIL}', '${DSMR_PASSWORD}')
+  print('${DSMR_USER} created')
 else:
-	print('${DSMR_USER} already exists')
+  print('${DSMR_USER} already exists')
 PYTHON"
 
 # Run supervisor
