c8ae519e943004d037ec97c7126651e6c816c14f
Bram van Dartel <spam@rootrulez.com>
2018-11-28 13:48:17 +0100

    rewrite of run and add build script for multiple platform support

--- app/run.sh
+++ app/run.sh
@@ -1,58 +1,92 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
 
-set -eo pipefail
-COMMAND="$@"
 
-if [ -e '/dev/ttyUSB0' ] ; then chmod 666 /dev/ttyUSB* ; fi
+#---------------------------------------------------------------------------------------------------------------------------
+# VARIABLES
+#---------------------------------------------------------------------------------------------------------------------------
+: "${DEBUG:=false}"
+: "${COMMAND:=$@}"
+: "${DB_PORT:-5432}"
+: "${TIMER:=60}"
 
-rm -f /var/tmp/*.pid
 
-# Check if we're able to connect to the database instance
-# already. The port isn't required for postgresql.py but
-# it is added for the sake of completion.
-DB_PORT=${DB_PORT:-5432}
+#---------------------------------------------------------------------------------------------------------------------------
+# FUNCTIONS
+#---------------------------------------------------------------------------------------------------------------------------
+function _info () { echo -e "\\r[ \\033[00;34mINFO\\033[0m ] $*"; }
+function _warn () { echo -e "\\r\\033[2K[ \\033[0;33mWARN\\033[0m ] $*"; }
+function _error () { echo -e "\\r\\033[2K[ \\033[0;31mFAIL\\033[0m ] $*"; }
+function _debug () { echo -e "\\r[ \\033[00;37mDBUG\\033[0m ] $*"; }
 
-cmd=$(command -v pg_isready)
-cmd="$cmd -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t 1"
-
-timeout=60
-while ! $cmd >/dev/null 2>&1; do
-  timeout=$(expr $timeout - 1)
-  if [[ $timeout -eq 0 ]]; then
-    echo "Could not connect to database server. Aborting..."
-    return 1
+function _pre_reqs() {
+  _info "Checking if the DSMR web credential variables have been set..."
+  if [ -z "${DSMR_USER}" ] || [ -z "${DSMR_EMAIL}" ] || [ -z "${DSMR_PASSWORD}" ]; then
+    _error "DSMR web credentials not set. Exiting..."
+    exit 1
   fi
-  echo -n "."
-  sleep 1
-done
-echo
 
-# Run migrations
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+  _info "Fixing /dev/ttyUSB* security..."
+  [[ -e '/dev/ttyUSB0' ]] && chmod 666 /dev/ttyUSB*
 
-# Override command if needed - this allows you to run
-# python3 manage.py for example. Keep in mind that the
-# WORKDIR is set to /home/dsmr/app.
-if [ -n "$COMMAND" ]; then
-  echo "ENTRYPOINT: Executing override command"
-  exec $COMMAND
-fi
+  _info "Removing existing PID files..."
+  rm -f /var/tmp/*.pid
+}
 
-if [ -z "${DSMR_USER}" ] || [ -z "$DSMR_EMAIL" ] || [ -z "${DSMR_PASSWORD}" ]; then
-  echo "DSMR web credentials not set. Exiting."
-  exit 1
-fi
+function _override_entrypoint() {
+  if [ -n "${COMMAND}" ]; then
+    _info "ENTRYPOINT: Executing override command..."
+    exec "${COMMAND}"
+  fi
+}
 
-# Create an admin user
-python3 manage.py shell -i python << PYTHON
-from django.contrib.auth.models import User
-if not User.objects.filter(username='${DSMR_USER}'):
-  User.objects.create_superuser('${DSMR_USER}', '${DSMR_EMAIL}', '${DSMR_PASSWORD}')
-  print('${DSMR_USER} created')
-else:
-  print('${DSMR_USER} already exists')
+function _check_db_availability() {
+  _info "Verifying if Postgres in running..."
+  cmd=$(command -v pg_isready)
+  cmd="${cmd} -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -d ${DB_NAME} -t 1"
+  while ! "${cmd}" >/dev/null 2>&1; do
+    TIMER=$((TIMER-1))
+    sleep 1
+    if [[ "${TIMER}" -eq 0 ]]; then
+      _error "Could not connect to database server. Aborting..."
+      return 1
+    fi
+    echo -n "."
+  done
+}
+
+function _run_post_config() {
+  _info "Running post configuration..."
+  cmd=$(command -v python3)
+  "${cmd}" manage.py migrate --noinput
+  "${cmd}" manage.py collectstatic --noinput
+
+  "${cmd}" manage.py shell -i python << PYTHON
+  from django.contrib.auth.models import User
+  if not User.objects.filter(username='${DSMR_USER}'):
+    User.objects.create_superuser('${DSMR_USER}', '${DSMR_EMAIL}', '${DSMR_PASSWORD}')
+    print('${DSMR_USER} created')
+  else:
+    print('${DSMR_USER} already exists')
 PYTHON
+}
 
-# Run supervisor
-/usr/bin/supervisord -n
+function _start_supervisord() {
+  _info "Starting supervisord..."
+  cmd=$(command -v supervisord)
+  "${cmd}" -n
+}
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# MAIN
+#---------------------------------------------------------------------------------------------------------------------------
+[[ "${DEBUG}" == 'true' ]] && set -o xtrace
+
+_pre_reqs
+_override_entrypoint
+_check_db_availability
+_run_post_config
+_start_supervisord
---
+++ build.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#---------------------------------------------------------------------------------------------------------------------------
+# VARIABLES
+#---------------------------------------------------------------------------------------------------------------------------
+: "${DEBUG:=false}"
+: "${ARCH_ARR:=amd64 arm32v6 arm64v8}"
+: "${DSMR_GIT_REPO:=dennissiemensma/dsmr-reader}"
+: "${QEMU_GIT_REPO:=multiarch/qemu-user-static}"
+: "${DEBUG:=false}"
+
+dsmr_release=$(curl -Ssl "https://api.github.com/repos/${DSMR_GIT_REPO}/releases/latest" | jq -r .tag_name)
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# FUNCTIONS
+#---------------------------------------------------------------------------------------------------------------------------
+function _debug () { printf "\\r[ \\033[00;37mDBUG\\033[0m ] %s\\n" "$@"; }
+function _info () { printf "\\r[ \\033[00;34mINFO\\033[0m ] %s\\n" "$@"; }
+function _warn () { printf "\\r\\033[2K[ \\033[0;33mWARN\\033[0m ] %s\\n" "$@"; }
+function _error () { printf "\\r\\033[2K[ \\033[0;31mFAIL\\033[0m ] %s\\n" "$@"; }
+
+function _pre_reqs() {
+  _info "Creating temporary directory..."
+  mkdir -p ./tmp/{dsmr,qemu}
+}
+
+function _dmsr_release() {
+  dsmr_release=$(curl -Ssl "https://api.github.com/repos/${DSMR_GIT_REPO}/releases/latest" | jq -r .tag_name)
+  _info "Using latest DSMR release: ${dsmr_release}."
+  pushd ./tmp/dsmr
+  wget -N https://github.com/"${DSMR_GIT_REPO}"/archive/"${dsmr_release}".tar.gz
+  tar -xf "${dsmr_release}".tar.gz --strip-components=1
+  #mv dsmr-reader-"${dsmr_release}" dsmr
+  rm -rf "${dsmr_release}".tar.gz
+  popd
+}
+
+function _update_qemu() {
+  qemu_release=$(curl -Ssl "https://api.github.com/repos/${QEMU_GIT_REPO}/releases/latest" | jq -r .tag_name)
+  _info "Downloading latest Qemu release: ${qemu_release}."
+  pushd ./tmp/qemu
+  for target_arch in ${ARCH_ARR}; do
+    wget -N https://github.com/"${QEMU_GIT_REPO}"/releases/download/"${qemu_release}"/x86_64_qemu-"${target_arch}"-static.tar.gz
+    tar -xf x86_64_qemu-"${target_arch}"-static.tar.gz
+    rm -rf x86_64_qemu-"${target_arch}"-static.tar.gz
+  done
+  popd
+}
+
+function _generate_docker_files() {
+  echo "$ARCH_ARR"
+  for docker_arch in ${ARCH_ARR}; do
+    case ${docker_arch} in
+      amd64       ) qemu_arch="x86_64" ;;
+      arm32v7     ) qemu_arch="arm" ;;
+      arm64v8     ) qemu_arch="aarch64" ;;
+      *)
+        _error "Unknown target architechture."
+        exit 1
+    esac
+    cp Dockerfile.cross Dockerfile."${docker_arch}"
+    sed -i '' "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile."${docker_arch}"
+    sed -i '' "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile."${docker_arch}"
+    if [[ ${docker_arch} == "amd64" ]]; then
+      #sed -i '' "s/__CROSS_\"].*//" Dockerfile."${docker_arch}"
+      #sed -i '' "/__CROSS_/d" Dockerfile."${docker_arch}"
+      sed -i '' "s/__CROSS_//g" Dockerfile."${docker_arch}"
+      sed -i '' "s/${docker_arch}/python/g" Dockerfile."${docker_arch}"
+    else
+      sed -i '' "s/__CROSS_//g" Dockerfile."${docker_arch}"
+    fi
+  done
+}
+
+function _build_docker_files() {
+  _info "Building Docker images..."
+  for docker_arch in ${ARCH_ARR}; do
+    _info "Building Docker images for: ${docker_arch}, release ${dsmr_release}."
+    docker build -f Dockerfile."${docker_arch}" -t xirixiz/dsmr-reader-docker:"${docker_arch}" .
+    docker tag xirixiz/dsmr-reader-docker:"${docker_arch}" xirixiz/dsmr-reader-docker:"${docker_arch}${dsmr_release}"
+    if [[ "${docker_arch}" == "amd64" ]]; then
+      docker tag xirixiz/dsmr-reader-docker:amd64 xirixiz/dsmr-reader-docker:latest
+      docker tag xirixiz/dsmr-reader-docker:amd64 xirixiz/dsmr-reader-docker:"${dsmr_release}"
+    fi
+  done
+}
+
+function _cleanup () {
+  _info "Cleaning up temporary files..."
+  rm -rf ./tmp
+  for docker_arch in ${ARCH_ARR}; do
+    [[ -f Dockerfile."${docker_arch}" ]] && rm -rf Dockerfile."${docker_arch}"
+    continue
+  done
+}
+
+#---------------------------------------------------------------------------------------------------------------------------
+# MAIN
+#---------------------------------------------------------------------------------------------------------------------------
+[[ "${DEBUG}" == 'true' ]] && set -o xtrace
+#set -o xtrace
+
+#_cleanup
+#_pre_reqs
+#_dmsr_release
+#_update_qemu
+_generate_docker_files
+_build_docker_files
+#_cleanup
