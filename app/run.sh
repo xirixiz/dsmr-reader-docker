b58f00cb5cc1096ed5981d30d0a9a9f9eb40cd50
Bram van Dartel <spam@rootrulez.com>
2019-05-06 13:50:30 +0200

    add loglevel support

--- app/run.sh
+++ app/run.sh
@@ -57,7 +57,7 @@ function _check_db_availability() {
 
 function _set_throttle() {
   if [[ -n "${DSMRREADER_BACKEND_SLEEP}" ]] ; then
-    if grep --quiet 'DSMRREADER_BACKEND_SLEEP' /dsmr/dsmrreader/settings.py; then
+    if grep 'DSMRREADER_BACKEND_SLEEP' /dsmr/dsmrreader/settings.py; then
       _info "Setting DSMRREADER_BACKEND_SLEEP already present, replacing values..."
       sed -i "s/DSMRREADER_BACKEND_SLEEP=.*/DSMRREADER_BACKEND_SLEEP=${DSMRREADER_BACKEND_SLEEP}/g"
     else
@@ -66,12 +66,28 @@ function _set_throttle() {
     fi
   fi
   if [[ -n "${DSMRREADER_DATALOGGER_SLEEP}" ]] ; then
-    if grep --quiet 'DSMRREADER_DATALOGGER_SLEEP' /dsmr/dsmrreader/settings.py; then
+    if grep 'DSMRREADER_DATALOGGER_SLEEP' /dsmr/dsmrreader/settings.py; then
       _info "Setting DSMRREADER_DATALOGGER_SLEEP already present, replacing values..."
-      sed -i "s/DSMRREADER_DATALOGGER_SLEEP=.*/DSMRREADER_DATALOGGER_SLEEP=${DSMRREADER_DATALOGGER_SLEEP}/g"     
+      sed -i "s/DSMRREADER_DATALOGGER_SLEEP=.*/DSMRREADER_DATALOGGER_SLEEP=${DSMRREADER_DATALOGGER_SLEEP}/g"
     else
       _info "Adding setting DSMRREADER_DATALOGGER_SLEEP..."
       sed -i "/# Default settings/a DSMRREADER_DATALOGGER_SLEEP=${DSMRREADER_DATALOGGER_SLEEP}" /dsmr/dsmrreader/settings.py
+    fi
+  fi
+}
+
+function _set_loglevel() {
+  if [[ -n "${DSMR_LOGLEVEL}" ]] ; then
+    if grep 'CUSTOM_LOGLEVEL' /dsmr/dsmrreader/settings.py; then
+      _info "Setting CUSTOM_LOGLEVEL already present, replacing values..."
+      sed -e "/CUSTOM_LOGLEVEL/,+2d" /dsmr/dsmrreader/settings.py
+      echo "# CUSTOM_LOGLEVEL" >> /dsmr/dsmrreader/settings.py
+      echo "LOGGING['loggers']['commands']['level'] = '${DSMR_LOGLEVEL}'" >> /dsmr/dsmrreader/settings.py
+    else
+      _info "Adding setting CUSTOM_LOGLEVEL..."
+      sed -e "/LOGGING/,+2d" /dsmr/dsmrreader/settings.py
+      echo "# CUSTOM_LOGLEVEL" >> /dsmr/dsmrreader/settings.py
+      echo "LOGGING['loggers']['commands']['level'] = '${DSMR_LOGLEVEL}'" >> /dsmr/dsmrreader/settings.py
     fi
   fi
 }
@@ -106,5 +122,6 @@ _pre_reqs
 _override_entrypoint
 _check_db_availability
 _set_throttle
+_set_loglevel
 _run_post_config
 _start_supervisord
