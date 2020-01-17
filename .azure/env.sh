09d5a486e8c1319b07c1a8e8b0e86ab756b0aab7
Bram van Dartel <spam@rootrulez.com>
2020-01-17 13:59:52 +0100

    Delete env.sh

--- .azure/env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "##vso[task.setvariable variable=DOCKER_USERNAME;issecret=true]${DOCKER_USERNAME}"
-echo "##vso[task.setvariable variable=DOCKER_PASSWORD;issecret=true]${DOCKER_PASSWORD}"
