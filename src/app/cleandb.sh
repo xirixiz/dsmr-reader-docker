342bd23f7ab8315a8315bb0226c4382bdef66c1f
Bram van Dartel <root@rootrulez.com>
2020-11-02 09:39:45 +0100

    structure docker files

--- src/app/cleandb.sh
+++ src/app/cleandb.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $1 = "-v" ]]; then
-    PGPASSWORD=${DB_PASS} /usr/bin/vacuumdb -f -v -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER}
+if [[ ${DB_ENGINE} == *"postgres"* ]]; then
+    if [[ $1 = "-v" ]]; then
+        PGPASSWORD=${DB_PASS} /usr/bin/vacuumdb -f -v -h ${DB_HOST} -p ${DB_PORT} -d ${DB_NAME} -U ${DB_USER}
+    else
+        PGPASSWORD=${DB_PASS} /usr/bin/vacuumdb -f -h ${DB_HOST} -p ${DB_PORT} -d ${DB_NAME} -U ${DB_USER}
+    fi
 else
-    PGPASSWORD=${DB_PASS} /usr/bin/vacuumdb -f -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER}
+    echo "The database engine is set to ${DB_ENGINE}, this script only works with a Postgresql database backend."
+    exit
 fi
