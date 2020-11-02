2c4cf87d5513cc1e092fcba91ef649404692a3e8
Bram van Dartel <root@rootrulez.com>
2020-11-02 08:25:01 +0100

    structure docker files

--- src/app-binaries/cleandb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $1 = "-v" ]]; then
-    PGPASSWORD=${DB_PASS} /usr/bin/vacuumdb -f -v -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER}
-else
-    PGPASSWORD=${DB_PASS} /usr/bin/vacuumdb -f -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER}
-fi
