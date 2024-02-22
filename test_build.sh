afa7563bd7167d07f62bed984ac8d81ca5795f07
goegol <info@hans-paulhemelaar.nl>
2024-02-22 13:59:34 +0100

    Update dsmrreader (#344)
    * Update to 5.11.0
    * Update to Alpine 3.19

--- test_build.sh
+++ test_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build . --build-arg DSMR_VERSION=5.10.4 --build-arg TARGETARCH="amd64" --build-arg TARGETVARIANT="" --build-arg QEMU_ARCH="x86_64" --build-arg DOCKER_TARGET_RELEASE=2099.09.09 -t dsmr_test_image
+docker build . --build-arg DSMR_VERSION=5.11.0 --build-arg TARGETARCH="amd64" --build-arg TARGETVARIANT="" --build-arg QEMU_ARCH="x86_64" --build-arg DOCKER_TARGET_RELEASE=2099.09.09 -t dsmr_test_image
