9aff67febd30455caf1bc9f9de476b15f3fc6d95
Bram van Dartel <bram.van.dartel@outlook.com>
2026-01-04 15:27:25 +0100

    update

--- rootfs/app/cleandb.sh
+++ rootfs/app/cleandb.sh
@@ -8,6 +8,6 @@ if [[ "${DJANGO_DATABASE_ENGINE}" == *"postgres"* ]]; then
         PGPASSWORD="${DJANGO_DATABASE_PASSWORD}" /usr/bin/vacuumdb -f -h "${DJANGO_DATABASE_HOST}" -p "${DJANGO_DATABASE_PORT}" -d "${DJANGO_DATABASE_NAME}" -U "${DJANGO_DATABASE_USER}"
     fi
 else
-    echo "The database engine is set to ${DJANGO_DATABASE_ENGINE}, this script only works with a Postgresql database backend."
+    echo "The database engine is set to ${DJANGO_DATABASE_ENGINE}, this script only works with a Postgresql database backend"
     exit 1
 fi
